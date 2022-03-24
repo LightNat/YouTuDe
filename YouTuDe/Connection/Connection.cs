420ce14368a86c060d3aa0245340408ed8758378
LightNat <91188229+LightNat@users.noreply.github.com>
2022-03-24 20:46:47 +0800

    Update Connection.cs

--- YouTuDe/Connection/Connection.cs
+++ YouTuDe/Connection/Connection.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 namespace YouTuDe.Connection
 {
     class Connection
-    {
+    {//Hello There
         public static SqlConnection conn;
         private static string connect = "Data Source=DESKTOP-GQFB644;Initial Catalog=YouTuDe;Integrated Security=True";
 
